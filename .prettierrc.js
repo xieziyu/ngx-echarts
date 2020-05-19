29722ad753eb9107f01ba869cb07fc5d4818cbed
xieziyu <xieziyu@iftech.io>
2020-05-19 11:42:45 +0800

    feat: refactored NgxEchartsModule and demo pages

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  useTabs: false,
+}
--- projects/ngx-echarts/core/src/public_api.ts
+++
@@ -1,5 +0,0 @@
-/*
- * Public API Surface of ngx-echarts-core
- */
-
-export * from './lib/ngx-echarts-core.module';
