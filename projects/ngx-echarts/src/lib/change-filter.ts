3619e8a31e61784818fcd4af052972cf538d9d2a
Xie, Ziyu <xieziyu@iftech.io>
2020-05-19 11:53:17 +0800

    ci: fix (#233)

--- projects/ngx-echarts/src/lib/change-filter.ts
+++ projects/ngx-echarts/src/lib/change-filter.ts
@@ -2,15 +2,15 @@ import { SimpleChanges } from '@angular/core';
 import { Observable, of, EMPTY } from 'rxjs';
 
 export class ChangeFilter {
-  constructor(private _changes: SimpleChanges) { }
+  constructor(private changes: SimpleChanges) { }
 
   static of(changes: SimpleChanges) {
     return new ChangeFilter(changes);
   }
 
   notEmpty<T>(key: string): Observable<T> {
-    if (this._changes[key]) {
-      const value: T = this._changes[key].currentValue;
+    if (this.changes[key]) {
+      const value: T = this.changes[key].currentValue;
 
       if (value !== undefined && value !== null) {
         return of(value);
@@ -20,24 +20,24 @@ export class ChangeFilter {
   }
 
   has<T>(key: string): Observable<T> {
-    if (this._changes[key]) {
-      const value: T = this._changes[key].currentValue;
+    if (this.changes[key]) {
+      const value: T = this.changes[key].currentValue;
       return of(value);
     }
     return EMPTY;
   }
 
   notFirst<T>(key: string): Observable<T> {
-    if (this._changes[key] && !this._changes[key].isFirstChange()) {
-      const value: T = this._changes[key].currentValue;
+    if (this.changes[key] && !this.changes[key].isFirstChange()) {
+      const value: T = this.changes[key].currentValue;
       return of(value);
     }
     return EMPTY;
   }
 
   notFirstAndEmpty<T>(key: string): Observable<T> {
-    if (this._changes[key] && !this._changes[key].isFirstChange()) {
-      const value: T = this._changes[key].currentValue;
+    if (this.changes[key] && !this.changes[key].isFirstChange()) {
+      const value: T = this.changes[key].currentValue;
 
       if (value !== undefined && value !== null) {
         return of(value);
