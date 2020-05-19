3abf74e70cc34b44fab06fd8068ba3218b1d0b58
xieziyu <xieziyu@iftech.io>
2020-05-19 18:57:55 +0800

    feat: add draggable chart

--- src/app/pages/demo-advanced/demo-advanced.module.ts
+++ src/app/pages/demo-advanced/demo-advanced.module.ts
@@ -5,9 +5,10 @@ import { SharedModule } from '../../shared/shared.module';
 import { DemoAdvancedRoutingModule } from './demo-advanced-routing.module';
 import { AdvancedComponent } from './advanced/advanced.component';
 import { ConnectChartsComponent } from './advanced/connect-charts/connect-charts.component';
+import { LineDraggableComponent } from './advanced/line-draggable/line-draggable.component';
 
 @NgModule({
-  declarations: [AdvancedComponent, ConnectChartsComponent],
+  declarations: [AdvancedComponent, ConnectChartsComponent, LineDraggableComponent],
   imports: [CommonModule, SharedModule, NgxEchartsModule, DemoAdvancedRoutingModule],
 })
 export class DemoAdvancedModule {}
